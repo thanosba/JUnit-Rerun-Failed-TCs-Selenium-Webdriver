e2648782209e975be1119ad424d7d4b144d82651
Athanasios Balanikas <athanasios.balanikas@yahoo.gr>
2018-07-28 00:28:54 +0300

    Add files via upload

---
+++ src/main/java/TestRule.java
@@ -0,0 +1,37 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class TestRule {
+    static WebDriver driver;
+    final private String URL = "https://www.google.com";
+    private ITestResult result;
+
+    @BeforeClass
+    public static void setupTest(){
+        driver = new ChromeDriver();
+    }
+
+    @Rule
+    public Retry retryRule = new Retry(4);
+
+
+    @Test
+    public void getURLExample() {
+        driver.get(URL);
+        assertThat(driver.getTitle(), is("Ggle"));
+    }
+    @AfterClass
+    public static void verification(){
+        driver.close();
+        driver.quit();
+    }
+}
